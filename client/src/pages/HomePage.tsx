429512fb62b958105019c9a6bf0c9b3695392227
melonadawit <melonadawit71@gmail.com>
2025-12-10 04:47:39 +0300

    Remove unused variables

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useAuth } from '../context/AuthContext';
 import Lobby from './Lobby';
 
 const HomePage: React.FC = () => {
-    const { user, isLoading } = useAuth();
-
     // Always show Lobby - authentication happens in background
     // This gives instant load experience
     return <Lobby />;
