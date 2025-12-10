81ddef029a847b54067d38451f2ab86f45686b5d
melonadawit <melonadawit71@gmail.com>
2025-12-10 04:29:47 +0300

    Remove unused imports from HomePage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import Lobby from './Lobby';
 
